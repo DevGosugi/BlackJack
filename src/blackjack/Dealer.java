67aa5713bd24402235bd2da6737a4a0b3e6d22d5
DevGosugi <goki.sugimura89@gmail.com>
2020-06-10 07:06:59 +0900

    Remove @Override from needCard(wantCard). Override addScore.

--- src/blackjack/Dealer.java
+++ src/blackjack/Dealer.java
@@ -1,30 +1,68 @@
 package blackjack;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Dealer extends Player {
     private int minScore;
-    Dealer(String name, int minScore) {
+    public Dealer(String name, int minScore) {
         super(name);
         this.minScore = minScore;
     }
 
-    @Override
-    public boolean wantCard(int max_score) {
+    public boolean needCard(int max_score) {
         if(this.hands.size() == 0) {
             return true;
         }
-        if(this.score < this.minScore) { // this. // super.
+        if(this.score < this.minScore) {
             // Dealerの最低点を下回っている場合は必ずもう1枚取るようにする
             return true;
         }
-        if(this.score > max_score) {
+        if(this.score >= minScore) { // max_score
+            this.stopDrawing = true;
             return false;
         }
 
         // もう1枚取るか判定
 //        if()
         return true;
+    }
+
+    @Override
+    protected void addScore(Card card, int max_score) {
+        Integer[] points = card.getNum().getPoints();
+        if(points.length == 1) {
+            this.score += card.getNum().getPoints()[0];
+        } else {
+            // 2つ以上のポイントから選択できるカードだった場合
+            // 今回はエースカードだった場合
+            /**
+             *  - pointsを大きい順にソート
+             *  - 大きい方から加算して検証
+             *      - バーストしていたらダメ
+             *      - 残りの最大を選択？
+             *          - 残りの最小でも結局バーストしてたらその最小を選んでエンド
+             *          - サブクラスDealerに至ってはminScore以上max_score以下になるものを最優先で選択だが、どの道max_score以下になれば良い
+             */
+            Arrays.sort(points);
+            for(int i = points.length - 1; 0 <= i; i--) {
+                if(i == 0) {
+                    this.score += points[i];
+                    break;
+                }
+                if(this.score + points[i] > max_score) {
+                    continue;
+                }
+                this.score += points[i];
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void printStatus() {
+        System.out.println("[Dealer(" + getName() + ")]");
+        System.out.println("score: " + getScore());
+        System.out.println("hands: " + getStrHands());
+        System.out.println("--------------------------------------------");
     }
 }
