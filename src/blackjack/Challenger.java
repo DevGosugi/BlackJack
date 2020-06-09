713108b23051a673d419b2ec72fc1f3831642c51
DevGosugi <goki.sugimura89@gmail.com>
2020-06-10 07:04:46 +0900

    Enable to System.in

--- src/blackjack/Challenger.java
+++ src/blackjack/Challenger.java
@@ -1,23 +1,63 @@
 package blackjack;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Challenger extends Player {
-    Challenger(String name) {
+    public Challenger(String name) {
         super(name);
     }
 
-    @Override
-    public boolean wantCard(int max_score) {
+    public boolean needCard() {
         if(this.hands.size() == 0) {
             return true;
         }
-        if(this.score > max_score) {
-            return false;
+        boolean needCard = false;
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.print("Do you want to draw?(y/n): ");
+            String input = sc.next();
+            if(input.equals("y")) {
+                needCard = true;
+                break;
+            }
+            if(input.equals("n")) {
+                needCard = false;
+                this.stopDrawing = true;
+                break;
+            }
+            System.out.println("Invalid input!!!");
         }
+        return needCard;
+    }
 
-        // もう1枚取るか判定
-//        if()
-        return true;
+    @Override
+    protected void addScore(Card card, int max_score) {
+        Integer[] points = card.getNum().getPoints();
+        if(points.length == 1) {
+            this.score += card.getNum().getPoints()[0];
+        } else {
+            System.out.println("You drew [" + card.getType() + "(" + card.getNum() + ")]" + "!");
+            Scanner sc = new Scanner(System.in);
+            int selectedNum = 0;
+            while(true) {
+                System.out.print("Select number to add from " + card.getNum().getStrPoints() + ": ");
+                try {
+                    selectedNum = Integer.parseInt(sc.next());
+                    if(Arrays.asList(points).contains(selectedNum)) {
+                        break;
+                    }
+                } catch(NumberFormatException e) {}
+                System.out.println("Invalid input!!!");
+            }
+            this.score += selectedNum;
+        }
+    }
+
+    @Override
+    public void printStatus() {
+        System.out.println("[Challenger(" + getName() + ")]");
+        System.out.println("score: " + getScore());
+        System.out.println("hands: " + getStrHands());
     }
 }
