0df8360199507abb575de02d95eafc13fc3d1f6b
DevGosugi <goki.sugimura89@gmail.com>
2020-06-10 07:13:37 +0900

    Fix commentout

--- src/blackjack/typedefs/CardNum.java
+++ src/blackjack/typedefs/CardNum.java
@@ -3,9 +3,11 @@ package blackjack.typedefs;
 /**
  *  急ぎ実装したが、CardTypeとCardNumがグローバルになっているのがなんかキモい
  *  【影響範囲】
- *  ・Card
- *  ・Challenger
- *  ・Dealer
+ *  ・blackjack.Card
+ *  ・blackjack.Challenger
+ *  ・blackjack.Dealer
+ *  ・test.Test
+ *  ・test.blackjack.Blackjack
  */
 // 使うカードの定義
 public enum CardNum {
