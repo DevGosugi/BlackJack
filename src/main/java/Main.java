141d1b4c2bc7d14daf0d2fd2cd184134102970b9
DevGosugi <goki.sugimura89@gmail.com>
2020-06-11 06:28:42 +0900

    Update commentout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,10 @@ public class Main {
 
 /**
  *  - 講師の[サンプルコード](https://github.com/MasatoshiTada/blackjack/tree/master/src/main/java/com/example)を見ての改善点
- *      - CardはEnumで
- *      - CardDeckクラス、Handsクラスを作る
- *      - maxScoreとdealerMinScoreは固定値なのでそれぞれ必要なクラスの中で定数として持たせる
+ *      - [ ] CardはEnumで
+ *      - [ ] CardDeckクラス、Handsクラスを作る
+ *      - [ ] maxScoreとdealerMinScoreは固定値なのでそれぞれ必要なクラスの中で定数として持たせる
  *          - maxScoreが本当に必要なクラスはどこか、よく考える(講師はHandsに持たせ、スコア判定やカード選択で用いている)
- *      - Challenger.needCard()の例外潰ししない。メッセージ出力を実装する
- *      - 「どちらかがバーストしたらゲーム終了」の実装…と思ったけど、2人以上だった場合はこれは不要かな？
+ *      - [x] Challenger.needCard()の例外潰ししない。メッセージ出力を実装する
+ *      - [ ] 「どちらかがバーストしたらゲーム終了」の実装…と思ったけど、2人以上だった場合はこれは不要かな？
  */
